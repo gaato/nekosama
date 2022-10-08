c3ed423b21262aee4c8ea18b489b3873195c13da
gaato <gart@outlook.jp>
2022-10-09 01:31:25 +0900

    add role commands

--- bot/__main__.py
+++ bot/__main__.py
@@ -9,6 +9,7 @@ from .views import RoleSelectView
 
 load_dotenv()
 intents = discord.Intents.default()
+intents.members = True
 bot = commands.Bot(intents=intents)
 
 
@@ -18,10 +19,35 @@ async def on_ready():
     print("------")
     bot.add_view(RoleSelectView(bot))
 
-
-@bot.command()
+@bot.slash_command()
 async def ping(ctx):
-    await ctx.send("pong")
+    await ctx.respond(f"Pong! ({bot.latency*1000}ms)")
+
+
+role = bot.create_group('role', description='Role commands')
+
+@role.command(
+    name='list',
+    description='ロール一覧を表示します',
+)
+async def role_list(ctx: discord.ApplicationContext):
+    embed = discord.Embed(
+        title='ロール一覧',
+        description='\n'.join([role.mention for role in ctx.guild.roles]),
+    )
+    await ctx.respond(embed=embed)
+
+
+@role.command(
+    name='members',
+    description='ロールのメンバー一覧を表示します',
+)
+async def role_members(ctx: discord.ApplicationContext, role: discord.Role):
+    embed = discord.Embed(
+        title=f'{role.name}のメンバー一覧',
+        description='\n'.join([member.mention for member in role.members]),
+    )
+    await ctx.respond(embed=embed)
 
 
 bot.run(os.environ.get('DISCORD_TOKEN'))

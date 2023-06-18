62128f72dd836634f8aa261f7b38e810b9b87428
gaato <g.furuya@gaato.net>
2023-06-19 00:04:16 +0900

    メンションとリンクをエスケープ

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import re
 import uuid
 from collections import OrderedDict
 from typing import List, Optional
@@ -128,6 +129,8 @@ async def on_message(message: discord.Message):
         translated_text, status = await translate(message.content, src='en', dest='ja')
     if status != 200:
         return
+    translated_text = re.sub(r'<@!?(\d+)>', lambda m: guild.get_member(int(m.group(1))).display_name, translated_text)
+    translated_text = re.sub(r'(?<!<)(https?://\S+)(?!>)', r'<\1>', translated_text)
     sent_message = await message.reply(translated_text, mention_author=False)
     await sent_message.add_reaction('🗑️')
 
